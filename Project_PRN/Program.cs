027f8e215f871bf5fb346cc5e4e4353ec492c864
TaBiLo <tblong2105@gmail.com>
2021-03-08 14:25:46 +0700

    add DTO and DAO

--- Project_PRN/Program.cs
+++ Project_PRN/Program.cs
@@ -13,7 +13,7 @@ namespace Project_PRN {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new LoginForm());
         }
     }
 }
