e1f362ada6ecca00a9bfc91283f38b5b2245a275
nn1401 <62437609+nn1401@users.noreply.github.com>
2021-03-08 14:42:15 +0700

    Update Program.cs

--- Project_PRN/Program.cs
+++ Project_PRN/Program.cs
@@ -13,7 +13,7 @@ namespace Project_PRN {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new MainForm());
         }
     }
 }
